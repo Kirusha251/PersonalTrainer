b03954c671785bccd93bf84fc838a4b285ab1012
Kirill Achramionok <mtanl8957@gmail.com>
2017-03-01 10:55:31 +0300

    Soluition
    add project solution

---
+++ PersonalTrainerWeb/src/main/java/by/achramionok/PersonalTrainerWebApplication.java
@@ -0,0 +1,12 @@
+package by.achramionok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTrainerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTrainerWebApplication.class, args);
+	}
+}
