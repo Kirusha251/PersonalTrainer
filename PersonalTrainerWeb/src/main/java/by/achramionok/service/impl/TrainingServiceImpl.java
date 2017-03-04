b39fc1932dfa45cfc6cf4346bb06f1dd3ab387f4
Kirill Achramionok <mtanl8957@gmail.com>
2017-03-04 22:50:42 +0300

    Implementation
    TrainingService implementation add

---
+++ PersonalTrainerWeb/src/main/java/by/achramionok/service/impl/TrainingServiceImpl.java
@@ -0,0 +1,41 @@
+package by.achramionok.service.impl;
+
+import by.achramionok.model.Training;
+import by.achramionok.repository.TrainingRepository;
+import by.achramionok.service.TrainingService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collection;
+
+/**
+ * Created by Kirill on 04.03.2017.
+ */
+public class TrainingServiceImpl implements TrainingService {
+    @Autowired
+    TrainingRepository trainingRepository;
+
+    @Override
+    public Training findByIdTraining(Integer id) {
+        return trainingRepository.findByIdTraining(id);
+    }
+
+    @Override
+    public Collection<Training> findAllByIdUser(Integer idUser) {
+        return findAllByIdUser(idUser);
+    }
+
+    @Override
+    public void save(Training training) {
+        trainingRepository.save(training);
+    }
+
+    @Override
+    public void deleteByIdTraining(Integer id) {
+        trainingRepository.deleteByIdTraining(id);
+    }
+
+    @Override
+    public Collection<Training> findAll() {
+        return trainingRepository.findAll();
+    }
+}
