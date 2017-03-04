31bfaa621454e7f528927c0ce9254cab7b18af04
Kirill Achramionok <mtanl8957@gmail.com>
2017-03-04 22:54:16 +0300

    Implementation
    Add ExerciseService implementation

---
+++ PersonalTrainerWeb/src/main/java/by/achramionok/service/impl/ExerciseServiceImpl.java
@@ -0,0 +1,41 @@
+package by.achramionok.service.impl;
+
+import by.achramionok.model.Exercise;
+import by.achramionok.repository.ExerciseRepository;
+import by.achramionok.service.ExerciseService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collection;
+
+/**
+ * Created by Kirill on 04.03.2017.
+ */
+public class ExerciseServiceImpl implements ExerciseService {
+    @Autowired
+    ExerciseRepository exerciseRepository;
+
+    @Override
+    public Exercise findByIdExercise(Integer id) {
+        return exerciseRepository.findByIdExercise(id);
+    }
+
+    @Override
+    public Exercise findByNameExercise(String name) {
+        return exerciseRepository.findByNameExercise(name);
+    }
+
+    @Override
+    public void save(Exercise exercise) {
+        exerciseRepository.save(exercise);
+    }
+
+    @Override
+    public void deleteByIdExercise(Integer id) {
+        exerciseRepository.deleteByIdExercise(id);
+    }
+
+    @Override
+    public Collection<Exercise> findAll() {
+        return exerciseRepository.findAll();
+    }
+}
