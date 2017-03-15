8fac2290f33ba9cc096f89cfb42941136d69d83e
Kirill Achramionok <mtanl8957@gmail.com>
2017-03-15 23:49:01 +0300

    Add exersice controller methods

--- PersonalTrainerWeb/src/main/java/by/achramionok/controller/ExerciseController.java
+++ PersonalTrainerWeb/src/main/java/by/achramionok/controller/ExerciseController.java
@@ -3,9 +3,8 @@ package by.achramionok.controller;
 import by.achramionok.model.Exercise;
 import by.achramionok.service.ExerciseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -13,12 +12,19 @@ import java.util.Collection;
  * Created by Kirill on 06.03.2017.
  */
 @RestController
+@RequestMapping(value = "exercise")
 public class ExerciseController {
     @Autowired
     private ExerciseService exerciseService;
 
-    @RequestMapping(name = "/exercise",method = RequestMethod.GET)
+    @RequestMapping(value = "/all",method = RequestMethod.GET)
     public Collection<Exercise> getAll(){
         return exerciseService.findAll();
     }
+
+    @RequestMapping(value = "/id/{id}", method = RequestMethod.GET)
+    public Exercise getById(@PathVariable String id){
+        return exerciseService.findById(Integer.valueOf(id));
+    }
+
 }
--- PersonalTrainerWeb/src/main/java/by/achramionok/model/User.java
+++ PersonalTrainerWeb/src/main/java/by/achramionok/model/User.java
@@ -1,6 +1,8 @@
 package by.achramionok.model;
 
 import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -9,109 +11,109 @@ import java.util.Date;
  */
 @Entity
 @Table(name = "user")//,catalog = "d6nc4cdvp2slen", schema = "public")
-public class User  implements Serializable {
+public class User implements Serializable {
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
     private int id;
+    @Basic
+    @Column(name = "name", length = 50)
     private String name;
+    @Basic
+    @Column(name = "role")
     private int role;
+    @Basic
+    @Column(name = "age")
     private int age;
+    @Basic
+    @Column(name = "email", length = 50)
     private String email;
+    @Basic
+    @Column(name = "password", length = 50)
     private String password;
+    @Basic
+    @Column(name = "sex", length = 20)
     private String sex;
+    @Basic
+    @Column(name = "birthday")
     private Date birthday;
+    @Basic
+    @Column(name = "registered_at")
     private Date registeredAt;
 
     public User() {
 
     }
 
-    @Id
-    @GeneratedValue
-    @Column(name = "id",nullable = false)
     public int getId() {
         return id;
     }
 
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
 
-    @Basic
-    @Column(name = "name", length = 50)
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    @Basic
-    @Column(name = "role")
-    public int getRole(){
+    public int getRole() {
         return role;
     }
 
-    public void setRole(int role){
+    public void setRole(int role) {
         this.role = role;
     }
 
-    @Basic
-    @Column(name = "age")
-    public int getAge(){
+    public int getAge() {
         return age;
     }
 
-    public void setAge(int age){
+    public void setAge(int age) {
         this.age = age;
     }
 
-    @Basic
-    @Column(name = "email", length = 50)
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email){
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    @Basic
-    @Column(name = "password", length = 50)
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password){
+    public void setPassword(String password) {
         this.password = password;
     }
 
-    @Basic
-    @Column(name = "sex", length = 20)
-    public String getSex(){
+    public String getSex() {
         return sex;
     }
 
-    public void setSex(String sex){
+    public void setSex(String sex) {
         this.sex = sex;
     }
 
-    @Basic
-    @Column(name = "birthday")
-    public Date getBirthday(){
+    public Date getBirthday() {
         return birthday;
     }
 
-    public void setBirthday(Date birthday){
+    public void setBirthday(Date birthday) {
         this.birthday = birthday;
     }
 
-    @Basic
-    @Column(name = "registered_at")
-    public Date getRegisteredAt(){
+    public Date getRegisteredAt() {
         return registeredAt;
     }
 
-    public void setRegisteredAt(Date registeredAt){
+    public void setRegisteredAt(Date registeredAt) {
         this.registeredAt = registeredAt;
     }
 }
